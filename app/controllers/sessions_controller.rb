a7c6eb87db9673bf24e5aa809692d3c5a3c33954
ichinose <ichinose@egrid.co.jp>
2017-10-23 07:24:13 +0000

    レイアウト調整

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'メールアドレスまたはパスワードの入力に誤りがあります。'
       render 'new'
     end
     
